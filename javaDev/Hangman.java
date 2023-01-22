2ff51d4bbccfaa82b1f7a7a99d661119cff4e95b
TheScientist1300 <109092276+TheScientist1300@users.noreply.github.com>
2023-01-22 21:11:13 +0200

    H.W

--- javaDev/Hangman.java
+++ javaDev/Hangman.java
@@ -3,23 +3,25 @@ package javaDev;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
-
-import org.w3c.dom.css.Counter;
-
 public class Hangman {
   public static void main(String[] args) throws IOException {
+
+
     // read words from a file 
     String filepath = "D:\\Users\\Tom\\Documents\\GitHub\\JavaCourse\\javaDev\\text2.txt";
     BufferedReader reader = null;
     String[] words = new String[100];
-
+    int numoflines = 0;
     try {
     reader = new BufferedReader(new FileReader(filepath));
+    String topic = reader.readLine();
+ System.out.println("The topic is: " + topic);
     String lines = reader.readLine();
-    int numoflines = 0;
     while ((lines =reader.readLine()) !=null) {
         if (numoflines ==0) {
      numoflines++;
@@ -41,16 +43,21 @@ public class Hangman {
      e.printStackTrace();
    }
 }
-    String topic;
-    String wordToGuess = words[(int) (Math.random() * words.length)];
+if(words.length == 0) {
+    System.out.println("File is Empty");
+    return;
+}
+Random random = new Random();
+int randomIndex = random.nextInt(numoflines -1);
+String wordToGuess = words[randomIndex];
     String hiddenWord = "";
     for (int i = 0; i < wordToGuess.length(); i++) {
       hiddenWord += "_";
     }
     int maxIncorrectGuesses = 6;
     int incorrectGuesses = 0;
+    HashSet<String> keywords = new HashSet();
     Scanner scanner = new Scanner(System.in);
-    // System.out.println("Topic: " + topic);
     while (incorrectGuesses < maxIncorrectGuesses) {
       System.out.println("Guess a letter in the word: " + hiddenWord); // 
       String guess = scanner.nextLine();
@@ -65,14 +72,87 @@ public class Hangman {
           System.out.println("You win! The word was " + wordToGuess);
           break;
         }
-      } else {
+      } else if(!keywords.contains(guess)){
         System.out.println("Incorrect.");
+        keywords.add(guess);
         incorrectGuesses++;
-      }
+    }else{
+        System.out.println("You already used this letter, try again.");
     }
+     
+        
+
+      if (incorrectGuesses == 0) {
+        System.out.println("  _____");
+        System.out.println(" |     |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+      }
+      if (incorrectGuesses == 1) {
+        System.out.println("  _____");
+        System.out.println(" |     |");
+        System.out.println(" |     O");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+      }
+      if (incorrectGuesses == 2) {
+        System.out.println("  _____");
+            System.out.println(" |     |");
+            System.out.println(" |     O");
+            System.out.println(" |     |");
+            System.out.println(" |     |");
+            System.out.println(" |");
+            System.out.println(" |");
+      }
+      if (incorrectGuesses == 3) {
+        System.out.println("  _____");
+        System.out.println(" |     |");
+        System.out.println(" |     O");
+        System.out.println(" |    \\|");
+        System.out.println(" |     |");
+        System.out.println(" |");
+        System.out.println(" |");
+      }
+      if (incorrectGuesses == 4) {
+        System.out.println("  _____");
+        System.out.println(" |     |");
+        System.out.println(" |     O");
+        System.out.println(" |    \\|/");
+        System.out.println(" |     |");
+        System.out.println(" |");
+        System.out.println(" |");
+      }
+      if (incorrectGuesses == 5) {
+        System.out.println("  _____");
+        System.out.println(" |     |");
+        System.out.println(" |     O");
+        System.out.println(" |    \\|/");
+        System.out.println(" |     |");
+        System.out.println(" |    /");
+        System.out.println(" |");
+      }
+    
     if (incorrectGuesses == maxIncorrectGuesses) {
+      System.out.println("  _____");
+      System.out.println(" |     |");
+      System.out.println(" |     O");
+      System.out.println(" |    \\|/");
+      System.out.println(" |     |");
+      System.out.println(" |    / \\");
+      System.out.println(" |");
       System.out.println("You lose. The word was " + wordToGuess);
     }
   }
 }
+}
+  
+
+
+// goal - if a guess was made twice don't include the second one (use an array or a set)
+
 
